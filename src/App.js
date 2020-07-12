42f4f32d280a3b9786b10c9caca4d79a9e1f80dd
toshaugh <toshaugh@comcast.net>
2020-07-11 20:21:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-      and i did
+            Edit <code>src/App.js</code> and save to reload. made new change...
           </p>
           <a
             className="App-link"
